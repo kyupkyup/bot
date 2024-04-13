c8b86e57e7f51a2b12e2019a964778f0d2761657
kyupkyup <rudduqdl@naver.com>
2024-04-13 23:22:33 +0900

    fix

--- library/cf.py
+++ library/cf.py
@@ -4,7 +4,7 @@ db_id='root' # [mysql ID를 넣어주세요]
 # db ip
 db_ip='localhost'
 # db 패스워드
-db_passwd='gymz84a9BHFQ!!' # [mysql password 를 넣어주세요]
+db_passwd='' # [mysql password 를 넣어주세요]
 
 # db port가 3306이 아닌 다른 port를 사용 하시는 분은 아래 변수에 포트에 맞게 수정하셔야합니다.
 db_port='3306'
